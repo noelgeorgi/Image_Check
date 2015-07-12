76c24d72b713a034c73b6218e83848b236b909ba
Noel Georgi <noelgeorgi@gmail.com>
2015-07-12 16:37:41 +0530

    Second Revision

--- app/src/main/java/com/example/noel/imagecheck/MainActivity.java
+++ app/src/main/java/com/example/noel/imagecheck/MainActivity.java
@@ -20,11 +20,13 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
     Button load_img;
-    ImageView img;
-    Bitmap bitmap;
+    ImageView img1,img2;
+    Bitmap bitmap[] = new Bitmap[2];
     ProgressDialog pDialog;
-    EditText edit;
+    EditText urlText1,urlText2;
     TextView text;
+    int img1ht,img2ht;
+    int img1wd,img2wd;
 
 
     @Override
@@ -32,23 +34,29 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         load_img = (Button)findViewById(R.id.enter);
-        img = (ImageView)findViewById(R.id.img);
-        edit = (EditText)findViewById(R.id.url1);
+        img1 = (ImageView)findViewById(R.id.imagedisplay1);
+        img2 = (ImageView)findViewById(R.id.imagedisplay2);
+        urlText1 = (EditText)findViewById(R.id.url1);
+        urlText2 = (EditText)findViewById(R.id.url2);
         text = (TextView)findViewById(R.id.urloutput);
         load_img.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
-                String name = edit.getText().toString();
-                text.append(name);
-                new LoadImage().execute(name);
+                String urlSring1 = urlText1.getText().toString();
+                String urlSring2 = urlText2.getText().toString();
+                //text.append(name);
+                bitmap[0]=null;
+                bitmap[1]=null;
+                new LoadImage().execute(urlSring1,urlSring2);
+
             }
         });
 
 
     }
-    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+    private class LoadImage extends AsyncTask<String,Void, Void> {
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -57,27 +65,32 @@ public class MainActivity extends Activity {
             pDialog.show();
 
         }
-        protected Bitmap doInBackground(String... args) {
-            try {
-                bitmap = BitmapFactory.decodeStream((InputStream)new URL(args[0]).getContent());
+        protected Void doInBackground(String... args) {
+                    if(args.length>0){
+                        for(int i=0;i<args.length;i++)
+                        {
+                    try {
+                        bitmap[i] = BitmapFactory.decodeStream((InputStream) new URL(args[i]).getContent());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }}}
 
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return bitmap;
+
+            return null;
         }
 
-        protected void onPostExecute(Bitmap image) {
+        protected void onPostExecute(Void result) {
 
-            if(image != null){
-                img.setImageBitmap(image);
+            if(bitmap[0] != null && bitmap[1] != null){
+                img1.setImageBitmap(bitmap[0]);
+                img2.setImageBitmap(bitmap[1]);
+                pDialog.dismiss();}
+
+
+            else{
+
                 pDialog.dismiss();
-
-
-            }else{
-
-                pDialog.dismiss();
-                Toast.makeText(MainActivity.this, "Image Does Not exist or Network Error", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Invalid URL or Network Error", Toast.LENGTH_SHORT).show();
 
             }
         }
