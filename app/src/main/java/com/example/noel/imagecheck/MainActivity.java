57d70b4ef2099e37358b2576af0bde92d5e8c4fe
Noel Georgi <noelgeorgi@gmail.com>
2015-07-12 19:15:37 +0530

    Third Revision

--- app/src/main/java/com/example/noel/imagecheck/MainActivity.java
+++ app/src/main/java/com/example/noel/imagecheck/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.noel.imagecheck;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -21,12 +22,14 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
     Button load_img;
     ImageView img1,img2;
-    Bitmap bitmap[] = new Bitmap[2];
+    Bitmap bitmap[] = new Bitmap[4];
     ProgressDialog pDialog;
     EditText urlText1,urlText2;
     TextView text;
-    int img1ht,img2ht;
-    int img1wd,img2wd;
+    int pixel1,pixel2,r1=0,g1=0,b1=0,r2=0,g2=0,b2=0;
+    long diff=0;
+    final int s1=90000,s2=90000;
+    String urlString1,urlString2;
 
 
     @Override
@@ -38,18 +41,21 @@ public class MainActivity extends Activity {
         img2 = (ImageView)findViewById(R.id.imagedisplay2);
         urlText1 = (EditText)findViewById(R.id.url1);
         urlText2 = (EditText)findViewById(R.id.url2);
-        text = (TextView)findViewById(R.id.urloutput);
+        text = (TextView)findViewById(R.id.percentval);
         load_img.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View arg0) {
                 // TODO Auto-generated method stub
-                String urlSring1 = urlText1.getText().toString();
-                String urlSring2 = urlText2.getText().toString();
-                //text.append(name);
+                urlString1="";
+                urlString2="";
+                urlString1 = urlText1.getText().toString();
+                urlString2 = urlText2.getText().toString();
                 bitmap[0]=null;
                 bitmap[1]=null;
-                new LoadImage().execute(urlSring1,urlSring2);
+                r1=0;g1=0;b1=0;r2=0;g2=0;b2=0;
+                diff=0;
+                new LoadImage().execute(urlString1,urlString2);
 
             }
         });
@@ -82,8 +88,33 @@ public class MainActivity extends Activity {
         protected void onPostExecute(Void result) {
 
             if(bitmap[0] != null && bitmap[1] != null){
-                img1.setImageBitmap(bitmap[0]);
-                img2.setImageBitmap(bitmap[1]);
+                bitmap[2]=Bitmap.createScaledBitmap(bitmap[0],300,300,true);
+                bitmap[3]=Bitmap.createScaledBitmap(bitmap[1],300,300,true);
+                img1.setImageBitmap(bitmap[2]);
+                img2.setImageBitmap(bitmap[3]);
+                for(int x=0;x<300;++x)
+                {
+                    for(int y=0;y<300;++y)
+                    {
+                        pixel1 = bitmap[2].getPixel(x,y);
+                        pixel2 = bitmap[3].getPixel(x,y);
+                        r1 += Color.red(pixel1);
+                        g1 += Color.green(pixel1);
+                        b1 += Color.blue(pixel1);
+                        r2 += Color.red(pixel2);
+                        g2 += Color.green(pixel2);
+                        b2 += Color.blue(pixel2);
+                        diff += Math.abs(r1 - r2);
+                        diff += Math.abs(g1 - g2);
+                        diff += Math.abs(b1-b2);
+
+                    }
+                }
+                double n = 90000*3;
+                double percent = 100 - diff*100/n/255.0;
+                String test=Double.toString(percent);
+                //text.addTextChangedListener();
+                text.append(test);
                 pDialog.dismiss();}
 
 
